7c9b30476fc69a8da6854ef15d94e7bfcf21e753
Arun <arunmundakkal003@gmail.com>
2025-01-04 11:18:01 +0530

    initial

---
+++ biolink/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App=()=>{
+  return(
+  <>
+    <div classNameName="Navbar">
+    
+    </div>
+    <input type="text" placeholder="enter the name"></input>
+  </>);
+}
+export default App;
